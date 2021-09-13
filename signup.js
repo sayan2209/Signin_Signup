298dce1d859fbe99cd9a24b8cabde7efe93864be
sayan2209 <90599108+sayan2209@users.noreply.github.com>
2021-09-13 12:10:24 +0530

    Add files via upload

---
+++ signup.js
@@ -0,0 +1,41 @@
+import { Card, Form, Button } from "react-bootstrap";
+import React, { useRef } from "react";
+
+export default function Signup() {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const passwordConfirmRef = useRef();
+
+  return (
+    <>
+
+      <Card>
+        <Card.Body>
+          <h2 className="text-center mb-4">Sign up</h2>
+          <Form>
+            <Form.Group id="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" ref={emailRef} required />
+            </Form.Group>
+            <Form.Group id="password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" ref={passwordRef} required />
+            </Form.Group>
+            <Form.Group id="paswordConfirm">
+              <Form.Label>Confirm Password</Form.Label>
+              <Form.Control type="password" ref={passwordConfirmRef} required />
+            </Form.Group>
+
+            <Button className="w-100 mt-4" type="submit">
+              Sign Up
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+
+      <div className="w-100 text-center mt-2">
+        Already have an account? Log in
+      </div>
+    </>
+  );
+}
